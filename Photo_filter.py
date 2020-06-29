f3cfd0a5a4af642aba707e90d0043b9875d8a7a7
Abhishek Narayana <63711207+Abhishek-Narayana@users.noreply.github.com>
2020-06-29 08:46:46 +0530

    Update Photo_filter
    Refactored the directory creation method.

--- Photo_filter.py
+++ Photo_filter.py
@@ -11,20 +11,14 @@ import shutil
 
 def create_folder_structure():
     """
-    Creates and returns the base dir if not exists
+    Creates dir structure and returns the sub dir
     """
     base_dir = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop', 'Photo Filter')
-    if not os.path.exists(base_dir):
-        os.mkdir(base_dir)
+    sub_dir = os.path.join(base_dir, 'Photos_' + datetime.now().strftime("%Y_%b_%d_%I_%M_%S"))
+    if not os.path.exists(sub_dir):
+        os.makedirs(sub_dir)
 
-    """
-    Creates instance dir for the current task
-    """
-    cur_dir_name = os.path.join(base_dir, 'Photos_' + datetime.now().strftime("%Y_%b_%d_%I_%M_%S"))
-    if not os.path.exists(cur_dir_name):
-        os.mkdir(cur_dir_name)
-
-    return cur_dir_name
+    return sub_dir
 
 
 def get_encoded_face(input_image):
@@ -37,17 +31,36 @@ def get_encoded_face(input_image):
     return fr.face_encodings(numpy_image)[0]
 
 
-def get_face(image):
-    image_numpy = fr.load_image_file(image)
+def get_pri_face():
+    """
+    returns primary face used for comparision
+    :return: primary face 
+    """
+    pri_image_loc = input("Enter primary face location ( complete path )\n")
+    while not os.path.exists(pri_image_loc):
+        pri_image_loc = input("Invalid file path, retry\n")
+    else:
+        print("Successfully read the primary face")
+
+    image_numpy = fr.load_image_file(pri_image_loc)
     if len(fr.face_locations(image_numpy)) >= 1:
         return fr.face_encodings(image_numpy)[0]
     return None
 
 
-def get_images(image_source_dir):
-    # returns all image files
+def get_sec_images():
+    """
+    returns all image files in the given folder location is valid
+    :return: list of images
+    """
+    sec_image_loc = input("Enter folder path having image collection to compare face\n")
+    while not os.path.exists(sec_image_loc):
+        sec_image_loc = input("Invalid file path, retry\n")
+    else:
+        print("Reading images....\n")
+
     images = []
-    for image in glob.iglob(os.path.join(image_source_dir, "*.jpg")):
+    for image in glob.iglob(os.path.join(sec_image_loc, "*.jpg")):
         images.append(image)
     return images
 
@@ -64,27 +77,20 @@ def compare_images(comparision_input):
 def main():
     # Read primary image used for comparision and verify that it contains at least 1 face.
     start = time.perf_counter()
-    pri_image = r'C:\Users\z003u7sd\Desktop\Images\megha.jpeg'
-
-    # replace with read primary face function
-    pri_face = get_face(pri_image)
+    pri_face = get_pri_face()
     if pri_face is None:
-        print("Invalid primary image")
+        print("Couldn't find face in given image.")
     else:
-        print("Successfully read the primary face")
         # Represents present working directory
         pwd = create_folder_structure()
 
-        # replace with reading collection of pics
-        image_source_dir = r'C:\Users\z003u7sd\Desktop\Images\dummy'
-
         comparision_data = []
-        for image in get_images(image_source_dir):
+        for image in get_sec_images():
             comparision_data.append([pri_face, image, pwd])
 
         with concurrent.futures.ProcessPoolExecutor() as executor:
             executor.map(compare_images, comparision_data)
-            
+
         end = time.perf_counter()
         print(f"Total Time taken {end - start}Second")
 
